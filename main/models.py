a0154d70995b7daf783cf835851286aaee2c0bc4
benyamin7798 <87536312+benyamin7798@users.noreply.github.com>
2024-06-16 18:22:48 +0430

    Product model and adding products in django admin are created

--- main/models.py
+++ main/models.py
@@ -1,16 +1,43 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
-def max_integer_validator(amount):
+def validate_ten_digits(amount):
     if amount>9999999999:
-        return False
-    return True
+        raise ValidationError('The amount cannot have more than 10 digits.')
+        
 
 
 class Product(models.Model):
+
+    HOT_DRINK = 'HD'
+    COLD_DRINK = 'CD'
+    CAKES = 'CAKES'
+
+    PRODUCT_CHOICES = [
+        (HOT_DRINK,'Hot Drink'),
+        (COLD_DRINK,'Cold Drink'),
+        (CAKES,'Cakes')
+    ]
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    sugar = models.IntegerField(validators=[max_integer_validator])
-    sugar = models.IntegerField(validators=[max_integer_validator])
-    sugar = models.IntegerField(validators=[max_integer_validator])
+
+    sugar = models.IntegerField(validators=[validate_ten_digits])
+    coffee = models.IntegerField(validators=[validate_ten_digits])
+    flour = models.IntegerField(validators=[validate_ten_digits])
+    chocolate = models.IntegerField(validators=[validate_ten_digits])
+
+    vertical = models.CharField(max_length=10,
+                                choices=PRODUCT_CHOICES,
+                                default=CAKES)
+    
+    price = models.IntegerField(validators=[validate_ten_digits])
+    
+    #image = models.ImageField(upload_to='/static/images')
+
+    def __str__(self):
+        return self.name
+
+    
     
 
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Product
 
 class HomePageView(TemplateView):
+    model = Product
     template_name = 'base.html'
