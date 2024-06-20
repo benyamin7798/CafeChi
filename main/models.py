83b760210b67fe7d152896124f9e7f12ed9fb801
sajad <saeidii.sajad@gmail.com>
2024-06-20 22:43:45 +0330

    slider upgraded

--- main/models.py
+++ main/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 
-class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=255)
-    sugar = models.IntegerField(validators=[MaxValueValidator(9999999999)])
+#class Product(models.Model):
+ #   id = models.AutoField(primary_key=True)
+  #  name = models.CharField(max_length=255)
+   # sugar = models.IntegerField(validators=[MaxValueValidator(9999999999)])
 
 #class Image(models.Model):
   #  picture = models.ImageField()
--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Image
+#from .models import Image
 
 def home(request):
     images = Image.objects.all()
