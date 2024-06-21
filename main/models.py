080a50063b118cb39c976a9d7dbed3f493ef6778
benyamin7798 <87536312+benyamin7798@users.noreply.github.com>
2024-06-21 20:18:19 +0430

    pic_database

--- main/models.py
+++ main/models.py
@@ -1,16 +1,35 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 def max_integer_validator(amount):
     if amount>9999999999:
-        return False
+        raise Exception('')
     return True
 
 
 class Product(models.Model):
+
+    HOT_DRINK = 'HD'
+    COLD_DRINK = 'CD'
+    CAKE = 'CAKE'
+    ICE_CREAM = 'IC'
+
+    PRODUCT_CHOICES = [
+        (HOT_DRINK, 'Hot Drink'),
+        (COLD_DRINK, 'Cold Drink'),
+        (CAKE, 'Cake'),
+        (ICE_CREAM,'Ice Cream')
+    ]
+
+
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    sugar = models.IntegerField(validators=[max_integer_validator])
-    sugar = models.IntegerField(validators=[max_integer_validator])
-    sugar = models.IntegerField(validators=[max_integer_validator])
+    sugar = models.IntegerField(validators=[max_integer_validator],default=0)
+    Coffee = models.IntegerField(validators=[max_integer_validator],default=0)
+    Flour = models.IntegerField(validators=[max_integer_validator],default=0)
+    Chocolate = models.IntegerField(validators=[max_integer_validator],default=0)
+    vertical = models.CharField(max_length=10,choices=PRODUCT_CHOICES,default=HOT_DRINK)
+    price = models.IntegerField(validators=[max_integer_validator],default=0)
+
+    image = models.ImageField(upload_to='../static/media')
     
 
