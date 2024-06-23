f86e537b3c74c72d17c0babe159687e94a952520
sajad <saeidii.sajad@gmail.com>
2024-06-23 22:05:23 +0330

    homepage html view

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'homepage.html'
