b84b39f36e05c060591b522c7ca41588af3f571c
sajad <saeidii.sajad@gmail.com>
2024-06-27 12:43:22 +0330

    style front of login page added

--- user_account/views.py
+++ user_account/views.py
@@ -1,3 +1,5 @@
+from django import forms
+
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import AuthenticationForm
@@ -5,16 +7,17 @@ from .forms import SignUpForm,CustomAuthenticationForm
 
 def custom_login_view(request):
     if request.method == 'POST':
-        form = CustomAuthenticationForm(request.POST)
-        if form.is_valid():
-            user = form.get_user()
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
             login(request, user)
             return redirect('homepage')
-    else:
-        form = CustomAuthenticationForm()
-        
-    return render(request, 'registration/login.html', {'form': form})
-
+        else:
+            # Handle the error case
+            context = {'error': 'Invalid credentials'}
+            return render(request, 'registration/login.html', context)
+    return render(request, 'registration/login.html')
 
 def sign_up_view(request):
     if request.method == 'POST':
@@ -26,3 +29,7 @@ def sign_up_view(request):
         form = SignUpForm()
     
     return render(request, 'registration/sign_up.html', {'form': form})
+
+class LoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
