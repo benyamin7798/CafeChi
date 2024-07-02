c265aecbbcc2df9aa297dacb98eed99a49b95e25
sajad <saeidii.sajad@gmail.com>
2024-07-02 23:19:03 +0330

    sign up button added font becomes persian

--- user_account/urls.py
+++ user_account/urls.py
@@ -1,11 +1,13 @@
 
 from django.urls import path
 from .views import (custom_login_view,
-                    sign_up_view)
+                    sign_up_view,
+                    signup)
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
             path('login/',custom_login_view,name='login'),
             path('logout/',LogoutView.as_view(),name='logout'),
             path('signup/',sign_up_view,name='signup'),
+            path('signup/', signup, name='signup'),
 ]
